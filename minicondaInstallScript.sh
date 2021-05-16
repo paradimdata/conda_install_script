c295d5aa99e34b648fcc247296e5ad85ccd13ca3
David Elbert <elbert@jhu.edu>
2021-05-16 10:08:38 -0400

    create branch to install nionswift with jupyter

--- minicondaInstallScript.sh
+++ minicondaInstallScript.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # miniconda and conda-forge installation script
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh #bash said that this was not the latest version
-bash Miniconda3-latest-Linux-x86_64.sh -b 
+wget --quiet --no-check-certificate https://repo.anaconda.com/miniconda/Miniconda3-py38_4.9.2-Linux-x86_64.sh  -O miniconda.sh 
+chmod +x /miniconda.sh
+bash miniconda.sh -b 
 export PATH="/home/$USER/miniconda3/bin:$PATH"
 conda config --set auto_activate_base false 
-conda create -n minimal_ds -y  
+conda create -n nionswift -y  
 conda update -n base -c default conda -y
 conda init bash
 source ~/.bashrc
 conda activate minimal_ds
 conda config --env --add channels conda-forge
 conda config --env --set channel_priority strict
-conda install pandas scikit-learn matplotlib notebook -y 
+conda install nionswift nionswift-tool pandas scikit-learn matplotlib notebook -y 
 echo done
